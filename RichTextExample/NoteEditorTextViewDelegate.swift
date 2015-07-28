842ca961a8bd3571c509ff6e0413d1e68c9729ca
Clay Tinnell <ctinnell@gaig.com>
2015-07-28 10:03:57 -0400

    Refactor functions into String extension
    This seems to provide improved readability and usability.

--- RichTextExample/NoteEditorTextViewDelegate.swift
+++ RichTextExample/NoteEditorTextViewDelegate.swift
@@ -11,28 +11,35 @@ import Foundation
 
 class NoteEditorTextViewDelegate: NSObject {
     
-    func characterIsAWordTerminator(text: String) -> Bool {
-        return text == " " || text == "." || text == "," || text == ":" || text == ";" || text == "\n" || text == "!" || text == "?"
-    }
-    
-    func stripSpecialCharacters(text: String) -> [String]? {
-        let strippedLowerCaseText = " ".join(text.componentsSeparatedByCharactersInSet(NSCharacterSet.letterCharacterSet().invertedSet)).lowercaseString
-        let trimmedWhiteSpace = strippedLowerCaseText.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).filter({!isEmpty($0)})
-        return trimmedWhiteSpace
-    }
-    
 }
 
 extension NoteEditorTextViewDelegate : UITextViewDelegate {
+    
     func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
-        
-        println("text changed: \(text)")
-        if characterIsAWordTerminator(text) {
-            println("word terminator")
-            if let strippedText = stripSpecialCharacters(textView.text) {
-                println(",".join(strippedText))
-            }
+        if text.isAWordTerminator() {
+            println(textView.text.stripSpecialCharacters().trimExtraWhiteSpace())
         }
         return true
     }
 }
+
+private extension String {
+    func isAWordTerminator() -> Bool {
+        return self == " " || self == "." || self == "," || self == ":" || self == ";" || self == "\n" || self == "!" || self == "?"
+    }
+    
+    func stripSpecialCharacters() -> String {
+        return " ".join(self.componentsSeparatedByCharactersInSet(NSCharacterSet.letterCharacterSet().invertedSet)).lowercaseString
+    }
+
+    func componentsSeparatedByWhiteSpace() -> [String] {
+        return self.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+    }
+    
+    func trimExtraWhiteSpace() -> String {
+        return " ".join(self.componentsSeparatedByWhiteSpace().filter({!$0.isEmpty}))
+    }
+}
+
+
+
